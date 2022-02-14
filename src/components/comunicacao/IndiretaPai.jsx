662237d9050129dd55ba3f02dc63a1d20a59a9fc
wallace azevedo <74993539+wallaceAzevedo@users.noreply.github.com>
2022-02-14 19:59:51 -0300

    ok

--- src/components/comunicacao/IndiretaPai.jsx
+++ src/components/comunicacao/IndiretaPai.jsx
@@ -3,12 +3,26 @@ import IndiretaFilho from "./IndiretaFilho";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props=> {
+    
+    //pasando os props no elemento pai aula 34 11 minutos e 40 segundos
+    let nome = '?'
+    let idade = 0
+    let nerd = false
+        
     // nome idade nerd
-    function fornecerInformacoes(nome, idade, nerd){
-        console.log(nome, idade, nerd)
+    function fornecerInformacoes(nomeParam, idadeParam, nerdParam){
+        nome = nomeParam
+        idade = idadeParam
+        nerd = nerdParam
+        
+        console.log(nomeParam, idadeParam, nerdParam)
     }
     return (
         <div>
+            <span>{nome} </span>
+            <span><strong> {idade} </strong></span>
+            <span> {nerd ? 'Verdadeiro' : 'Falso'}</span>
+
             <div>Pai</div>
             <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
         </div>
