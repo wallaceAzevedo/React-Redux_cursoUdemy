bb4dfcc32380123e22ad9153ed217db1cfbcbb1e
wallaceAzevedo <wallaceazevedo17@gmail.com>
2022-02-21 14:30:19 -0300

    first commit
    componentes criados

---
+++ navegacao/src/components/layout/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Content.css'
+
+const Content = props => (
+    <main className='Content'>
+        Component Content
+    </main>
+)
+
+export default Content
---
+++ navegacao/src/views/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+const App = props => (
+    <div className='App'>
+        Component App
+    </div>
+)
+
+export default App
